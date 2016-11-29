4f75f9e7f823ecb12ded8ce88086a0e7fc72f699
nguyen chi thanh <n.c.thanh0801@gmail.com>
2016-11-29 15:10:33 +0700

    admin edit delete word

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -1,6 +1,7 @@
 class Admin::WordsController < ApplicationController
   before_action :verify_login, :verify_admin
-  before_action :load_all_category, only: [:new, :create]
+  before_action :load_all_category, except: [:show, :index, :destroy]
+  before_action :load_word, except: [:index, :new, :create]
 
   def index
     params[:search] ||= ""
@@ -17,20 +18,40 @@ class Admin::WordsController < ApplicationController
     @word = Word.create word_params
     if @word.save
       flash[:success] = t "word_sucessful"
-      redirect_to :back
+      redirect_to admin_words_path
     else
       render :new
     end
   end
 
   def show
-    @word = Word.find_by id: params[:id]
     @answers = @word.answers
+  end
+
+  def edit
+  end
+
+  def update
+    if @word.update_attributes word_params
+      flash[:success] = t ".update_success"
+    else
+      flash[:notice] = t ".update_failed"
+    end
+    redirect_to admin_words_path
+  end
+
+  def destroy
+    if @word.destroy
+      flash[:success] = t "deleted"
+    else
+      flash[:danger] = t "fail_to_delete"
+    end
+    redirect_to :back
   end
 
   private
   def word_params
     params.require(:word).permit :category_id, :content,
-      answers_attributes: [:content, :is_correct, :_destroy]
+      answers_attributes: [:id, :content, :is_correct, :_destroy]
   end
 end
